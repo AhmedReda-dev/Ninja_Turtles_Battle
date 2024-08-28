ff390968c3896c480e7a010e4eb7f3165abdb331
christina magdy <christinamagdy2003@gmail.com>
2024-08-28 12:27:49 -0400

    updated game engine node

--- turtles_pkg/scripts/game_engine_node.py
+++ turtles_pkg/scripts/game_engine_node.py
@@ -1,6 +1,13 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose
+from std_msgs.msg import Bool
+
+turtle_one_health = 100
+turtle_one_attacks = 10
+turtle_two_health = 100
+turtle_two_attacks = 10
+
 
 turtles_positions = {'turtle1': Pose(), 'turtle2': Pose()}
 
@@ -9,6 +16,27 @@ def update_position(data, turtle_name):
     turtles_positions[turtle_name] = data
     #rospy.loginfo(f"New position for {turtle_name}: {turtles_positions[turtle_name]}")
 
+def update_health_turtle1 (msg):
+    if msg.data:
+        rospy.loginfo ("Attack detected")
+        turtle_one_attacks = turtle_one_attacks -1
+        turtle_one_health = turtle_one_health -50
+
+    else:
+        rospy.loginfo ("No Attack detected")
+
+def update_health_turtle2 (msg):
+    if msg.data:
+        rospy.loginfo ("Attack detected")
+        turtle_two_attacks = turtle_two_attacks -1
+        turtle_two_health = turtle_two_health -50
+
+    else:
+        rospy.loginfo ("No Attack detected")
+
+
+
+
 if __name__ == '__main__':
     rospy.init_node('game_engine')
     rospy.loginfo("Node has been started")
@@ -16,5 +44,7 @@ if __name__ == '__main__':
     # Subscribers
     rospy.Subscriber('/turtle1/pose', Pose, update_position, 'turtle1')
     rospy.Subscriber('/turtle2/pose', Pose, update_position, 'turtle2')
+    rospy.Subscriber('/turtle_one/is_attacking' , Bool , update_health_turtle1)
+    rospy.Subscriber('/turtle_two/is_attacking' , Bool , update_health_turtle2)
 
 rospy.spin()
